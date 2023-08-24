452d3034069f47f52c9b80353a95beee12ae5ba0
livinggod <daniel.rip.0229@gmail.com>
2023-08-24 15:28:49 +0330

    a better code

--- calculator.py
+++ calculator.py
@@ -1,22 +1,46 @@
-x=int (input("add your num :"))
-sign=input("chois the sign (+,//,-,*) :")
-y=int (input("add your num :"))
+def get_number_input(prompt):
+    while True:
+        try:
+            num = float(input(prompt))
+            return num
+        except ValueError:
+            print("Invalid input. Please enter a valid number.")
 
-def calculator():
-   
+def get_operator_input():
+    while True:
+        operator = input("Choose the operation (+, -, *, //): ")
+        if operator in ('+', '-', '*', '//'):
+            return operator
+        else:
+            print("Invalid operation. Please choose from +, -, *, //.")
 
-    if sign=="+":
-        t = x + y
-    elif sign=="-":
-        t = x - y
-    elif sign=="*":
-        t = x * y
-    elif sign=="//":
-        t = x // y   
+def calculator(x, y, operator):
+    if operator == "+":
+        result = x + y
+    elif operator == "-":
+        result = x - y
+    elif operator == "*":
+        result = x * y
+    elif operator == "//":
+        if y != 0:
+            result = x // y
+        else:
+            print("Division by zero is not allowed.")
+            return None
     else:
-        print("Invalid sign")
-        return
-    print(t)
-        
-    
-calculator()
+        print("Invalid operator")
+        return None
+    return result
+
+def main():
+    print("Welcome to the Pro Calculator!")
+    x = get_number_input("Enter the first number: ")
+    operator = get_operator_input()
+    y = get_number_input("Enter the second number: ")
+
+    result = calculator(x, y, operator)
+    if result is not None:
+        print("Result:", result)
+
+if __name__ == "__main__":
+    main()
